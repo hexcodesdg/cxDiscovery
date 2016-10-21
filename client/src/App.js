b5fae4ac17d219c67513f7bcf2c242c54d37c1d5
Arthelon <hsing.daniel@gmail.com>
2016-10-21 20:26:34 +0800

    moved from mui to rebass-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react';
-import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 class App extends Component {
-
-    static childContextTypes = {
-        muiTheme: React.PropTypes.object.isRequired
-    }
-
-    getChildContext() {
-        return {
-            muiTheme: getMuiTheme(baseTheme)
-        }
-    }
 
     render() {
         return (
--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,7 @@ import { Provider } from 'react-redux'
 import { Router, browserHistory } from 'react-router'
 import getRoutes from './routes'
 import configureStore from './store'
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 
 ReactDOM.render(
