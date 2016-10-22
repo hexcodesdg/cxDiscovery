902e28d8758753a8b26ace21d0e6f1483565f447
Arthelon <hsing.daniel@gmail.com>
2016-10-23 02:35:38 +0800

    Fixed bug where toggled favourite tags remained on current_tags list

--- client/src/components/Ad.js
+++ client/src/components/Ad.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import {Card, CardMedia, CardTitle, CardActions, CardText} from 'material-ui/Card'
 import IconButton from 'material-ui/IconButton'
+import FlatButton from 'material-ui/FlatButton'
 import TurnedIn from 'material-ui/svg-icons/action/turned-in'
 import TurnedInNot from 'material-ui/svg-icons/action/turned-in-not'
-import { Row } from 'react-inline-grid'
+import { Row, Cell } from 'react-inline-grid'
+import Dialog from 'material-ui/Dialog'
 import Chip from 'material-ui/Chip'
 
 export default class Ad extends Component {
 
+    state = {
+        modalVisible: false
+    }
+
     constructor(props) {
         super(props)
-
+        this.onModalClose = this.onModalClose.bind(this)
     }
 
     static propTypes = {
@@ -19,38 +25,67 @@ export default class Ad extends Component {
         body: React.PropTypes.string.isRequired,
         toggleSaved: React.PropTypes.func.isRequired,
         isSaved: React.PropTypes.bool.isRequired,
-        tags: React.PropTypes.array.isRequired
+        tags: React.PropTypes.array.isRequired,
+        handleClick: React.PropTypes.func.isRequired
+    }
+
+    onModalClose() {
+        this.setState({
+            modalVisible: false
+        })
     }
 
     render() {
         const imageStyle = {maxWidth: "600px", minWidth:"300px"}
+        const actions = <FlatButton primary={true} label="Close" onTouchTap={this.onModalClose}/>
 
         return (
             <Row is="center">
-                <Card
-                    initiallyExpanded={true}>
-                    <CardMedia>
-                        <img style={imageStyle} src={this.props.imageUrl}/>
-                    </CardMedia>
-                    <CardTitle title={this.props.title}/>
-                    <CardText>
-                        {this.props.body}
-                    </CardText>
-                    <CardActions>
-                        <Row>
-                            {this.props.tags.map(tag => {
-                                return <Chip>{tag}</Chip>
-                            })}
-                        </Row>
-                        <IconButton onClick={this.props.toggleSaved}>
-                            {this.props.isSaved ?
-                                <TurnedIn/>
-                            :
-                                <TurnedInNot/>
-                            }
-                        </IconButton>
-                    </CardActions>
-                </Card>
+                <div>
+                    <Card
+                        initiallyExpanded={true}>
+                        <CardMedia onClick={() => {
+                                this.setState({
+                                    modalVisible: true
+                                })
+                                this.props.handleClick()
+                        }}>
+                            <img style={imageStyle} src={this.props.imageUrl}/>
+                        </CardMedia>
+                        <CardTitle title={this.props.title}/>
+                        <CardText>
+                            {this.props.body.substring(0, 50)}
+                        </CardText>
+                        <CardActions>
+                            <Row>
+                                {this.props.tags.map(tag => {
+                                    return <Chip>{tag}</Chip>
+                                })}
+                            </Row>
+                            <IconButton onClick={this.props.toggleSaved}>
+                                {this.props.isSaved ?
+                                    <TurnedIn/>
+                                :
+                                    <TurnedInNot/>
+                                }
+                            </IconButton>
+                        </CardActions>
+                    </Card>
+                    <Dialog
+                      title={this.props.title}
+                      actions={actions}
+                      modal={true}
+                      contentStyle={{width: "100%"}}
+                      onRequestClose={this.onModalClose}
+                      open={this.state.modalVisible}
+                      autoScrollBodyContent={true}
+                    >
+                        <Cell is="middle 6">
+                            <img style={imageStyle} src={this.props.imageUrl}/>
+                        </Cell>
+                          {this.props.body}
+                    </Dialog>
+                </div>
             </Row>
         )
     }
